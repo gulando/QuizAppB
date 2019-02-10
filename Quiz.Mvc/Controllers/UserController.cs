88d640029685260062709f600197ccecad894a53
yourname <andranikghulyan.work@gmail.com>
2019-02-10 20:42:38 +0400

    minor changes.

--- Quiz.Mvc/Controllers/UserController.cs
+++ Quiz.Mvc/Controllers/UserController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -16,6 +19,7 @@ using QuizService;
 
 namespace QuizMvc.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         #region properties
@@ -41,6 +45,72 @@ namespace QuizMvc.Controllers
         
         #region actions
 
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            var users = _userService.Users;
+            var userDataList = _mapper.Map<IEnumerable<UserData>>(users);
+            return View(userDataList);
+        }
+        
+        [AllowAnonymous]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.CreateMode = false;
+            var user = _userService.GetUserByID(id);
+            var userData = _mapper.Map<UserData>(user);
+            return View("EditUser", userData);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(UserData userData)
+        {
+            var user = _mapper.Map<User>(userData);
+            _userService.Update(user, userData.Password);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [AllowAnonymous]
+        public IActionResult Create()
+        {
+            ViewBag.CreateMode = true;
+            return View("EditUser", new UserData());
+        }
+        
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Create(UserData userData)
+        {
+            var user = _mapper.Map<User>(userData);
+            _userService.Create(user,userData.Password);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _userService.DeleteUser(id);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(UserData userData)
+        {
+            if (ModelState.IsValid)
+            {
+                Authenticate(userData);
+            }
+
+            return null;
+        }
+        
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "User");
+        }
+        
         [AllowAnonymous]
         [HttpPost("authenticate")]
         public JsonResult Authenticate([FromBody]UserData userDto)
@@ -71,54 +141,6 @@ namespace QuizMvc.Controllers
                 user.LastName,
                 Token = tokenString
             });
-        }
-        
-        public IActionResult Index()
-        {
-            var users = _userService.Users;
-            var userDataList = _mapper.Map<IEnumerable<UserData>>(users);
-            return View(userDataList);
-        }
-
-        [HttpPost]
-        public IActionResult Delete(int id)
-        {
-            _userService.DeleteUser(id);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Edit(int id)
-        {
-            ViewBag.CreateMode = false;
-            var user = _userService.GetUserByID(id);
-            var userData = _mapper.Map<UserData>(user);
-            return View("EditUser", userData);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(UserData userData)
-        {
-            var user = _mapper.Map<User>(userData);
-            
-            _userService.Update(user, userData.Password);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Create()
-        {
-            ViewBag.CreateMode = true;
-            return View("EditUser", new UserData());
-        }
-        
-        [AllowAnonymous]
-        [HttpPost]
-        public IActionResult Create(UserData userData)
-        {
-            var user = _mapper.Map<User>(userData);
-            
-            _userService.Create(user,userData.Password);
-            
-            return RedirectToAction(nameof(Index));
         }
         
         #endregion
